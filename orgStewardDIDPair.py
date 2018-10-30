28841d56fac9ab5fc61807237891ced0060ad381
Madhur1997 <31137528+Madhur1997@users.noreply.github.com>
2018-10-30 23:09:47 +0530

    Add files via upload

--- orgStewardDIDPair.py
+++ orgStewardDIDPair.py
@@ -14,17 +14,13 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 async def run():
-	logger.info("Getting started -> started")
 
 	pool_name = 'pool1'
-	logger.info("Open Pool Ledger: {}".format(pool_name))
 	pool_genesis_txn_path = get_pool_genesis_txn_path(pool_name)
-	#print(pool_genesis_txn_path)
 	pool_config = json.dumps({"genesis_txn": str(pool_genesis_txn_path)})
-	#print(pool_config)
 
 
-	# Set protocol version 2 organization work with Indy Node 1.4
+	# Set protocol version 2 NSUT work with Indy Node 1.4
 	await pool.set_protocol_version(PROTOCOL_VERSION)
 
 	try:
@@ -35,45 +31,51 @@ async def run():
 	pool_handle = await pool.open_pool_ledger(pool_name, None)
 
 	logger.info("==============================")
-	logger.info("== Organization gets the wallet  ==")
+	logger.info("== NSUT creates a wallet  ==")
 	logger.info("------------------------------")
 
-	organization_wallet_config = json.dumps({"id": "organization_wallet"})
-	organization_wallet_credentials = json.dumps({"key": "organization_wallet_key"})
+	NSUT_wallet_config = json.dumps({"id": "NSUT_wallet"})
+	NSUT_wallet_credentials = json.dumps({"key": "NSUT_wallet_key"})
 
 	try:
-		await wallet.create_wallet(organization_wallet_config, organization_wallet_credentials)
+		await wallet.create_wallet(NSUT_wallet_config, NSUT_wallet_credentials)
 	except IndyError as ex:
 		if ex.error_code == ErrorCode.WalletAlreadyExistsError:
 	    		pass
-	organization_wallet = await wallet.open_wallet(organization_wallet_config, organization_wallet_credentials)
+	NSUT_wallet = await wallet.open_wallet(NSUT_wallet_config, NSUT_wallet_credentials)
 
-	logger.info("\"Organization\" -> Create and store in Wallet \"Organization Steward\" ")
-	(organization_steward_did, organization_steward_key) = await did.create_and_store_my_did(organization_wallet, "{}")
+	logger.info("\"NSUT\" -> Create and store in Wallet \"NSUT-Steward DID\" ")
+	(NSUT_steward_did, NSUT_steward_key) = await did.create_and_store_my_did(NSUT_wallet, "{}")
 
-	logger.info("\"Organization\" -> Get key for did from \"Steward\" connection request")
+	logger.info("\"NSUT\" -> Get key for did from \"Steward\" connection request")
 
-	fname = "connectionReqStewardOrg.txt"
+	fname = "connectionReqStewardNSUT.txt"
 	with open(fname,'r') as f:
 		connection_request = eval(f.readline())
-	print(connection_request)
-	steward_organization_verkey = await did.key_for_did(pool_handle, organization_wallet, connection_request['did'])
+	
+	steward_NSUT_verkey = await did.key_for_did(pool_handle, NSUT_wallet, connection_request['did'])
 
-	logger.info("\"Organization\" -> Anoncrypt connection response for \"Steward\" with \"Organization Steward\" DID, verkey and nonce")
+	logger.info("\"NSUT\" -> Anoncrypt connection response for \"Steward\" with \"NSUT-Steward\" DID, verkey and nonce")
 
-	connection_response = json.dumps({
-	'did': organization_steward_did,
-	'verkey': organization_steward_key,
+	'''connection_response = json.dumps({
+	'did': NSUT_steward_did,
+	'verkey': NSUT_steward_key,
 	'nonce': connection_request['nonce']
-	})
+	})'''
 
-	anoncrypted_connection_response = await crypto.anon_crypt(steward_organization_verkey, connection_response.encode('utf-8'))
 
-	logger.info("\"Organization\" -> Send anoncrypted connection response to \"Steward")
-	fname = "connectionRespOrgSteward.bin"
-	#print(anoncrypted_connection_response)
-	with open(fname,'wb') as f:
-		f.write(anoncrypted_connection_response)
+	anoncrypted_connection_response = {
+	'did': NSUT_steward_did,
+	'verkey': NSUT_steward_key,
+	'nonce': connection_request['nonce']
+	}
+
+	#anoncrypted_connection_response = await crypto.anon_crypt(steward_NSUT_verkey, connection_response.encode('utf-8'))
+
+	logger.info("\"NSUT\" -> Send anoncrypted connection response to \"Steward")
+	fname = "connectionRespNSUTSteward.txt"
+	with open(fname,'w') as f:
+		f.write(str(anoncrypted_connection_response))
 if __name__ == '__main__':
 	run_coroutine(run)
 	time.sleep(1)  
